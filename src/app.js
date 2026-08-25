1c55454c09d622956fe1a28d2a8e4cc96e6f307b
Aziz Alfauzi <azizalfauzi65@gmail.com>
2026-08-25 12:04:38 +0700

    menghapus koneksi pada database dan mengganti fungsi pemanggilan api pada nodejs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-const db = require("../config/db");
+const express = require("express");
+const app = express();
+const userRoutes = require("../routes/userRoutes");
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-    return;
-  }
-  console.log("Connected to the database mysql xampp");
+// Middleware parsing Body dari JSON request (Wajib!)
+app.use(express.json);
+
+// Jalur Utama (Prefix URL)
+app.use("/users", userRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
