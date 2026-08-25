d238c64decb862971b0b087e96d2eacb7204a923
Aziz Alfauzi <azizalfauzi65@gmail.com>
2026-08-25 12:25:51 +0700

    perubahan struktur pada app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,12 @@ const app = express();
 const userRoutes = require("../routes/userRoutes");
 
 // Middleware parsing Body dari JSON request (Wajib!)
-app.use(express.json);
+app.use(express.json());
 
 // Jalur Utama (Prefix URL)
-app.use("/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = 3000;
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
-
