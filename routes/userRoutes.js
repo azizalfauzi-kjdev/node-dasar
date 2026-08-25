ecb0fe048b8c20adbb9519101d79200af7fbe894
Aziz Alfauzi <azizalfauzi65@gmail.com>
2026-08-25 12:28:09 +0700

    penyesuaran pemanggilan pada file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const userController = require("../controllers/userControllers");
 
 // Definisi Rute URL
 router.get("/", userController.getAllUsers); // GET /api/users
