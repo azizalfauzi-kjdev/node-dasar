92c25e98d1b8394bba25678d997db4126ebf1883
Aziz Alfauzi <azizalfauzi65@gmail.com>
2026-08-25 12:25:12 +0700

    perubahan struktur pada routing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userControllers");
+const userController = require("../controllers/userController");
 
-// Define routes and link them to controller functions
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+// Definisi Rute URL
+router.get("/", userController.getAllUsers); // GET /api/users
+router.get("/:id", userController.getUserById); // GET /api/users/1
+router.post("/", userController.createUser); // POST /api/users
+router.put("/:id", userController.updateUser); // PUT /api/users/1
+router.delete("/:id", userController.deleteUser); // DELETE /api/users/1
 
 module.exports = router;
